8572fd4e16c93ca69ef81b3c189e65785f9dba05
mostafahany99 <114784979+mostafahany99@users.noreply.github.com>
2022-10-26 11:52:42 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+exports.Default_Prefix = "!";
+exports.Color = "blue";
+exports.Token = "MTAxOTY4NjYxMjM0NDUwODQ1Nw.GyQDbI.kkYR5ZwOtmq92atKFuCEBnGcjmtwSzC8St5jCI";
+exports.OwnerID = "639061475876405258";
